e65967caeb36f6925ac0a6d5b8dcf640217a2327
Daniel Honies <daniel.honies@gmail.com>
2015-09-21 14:40:34 +0200

    Initial Commit
    Project setup

---
+++ MissionControl/MissionControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MissionControl
+//
+//  Created by Daniel Honies on 21.09.15.
+//  Copyright © 2015 Daniel Honies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
