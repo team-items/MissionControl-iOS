9e04c5c06ceca0a1a20249229fbd326d82b59a2b
Daniel Honies <daniel.honies@gmail.com>
2015-10-04 20:25:57 +0200

    bugfix

--- MissionControl/MissionControl/MotorTableViewController.swift
+++ MissionControl/MissionControl/MotorTableViewController.swift
@@ -66,6 +66,7 @@ class MotorTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            motors.removeFirst()
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
